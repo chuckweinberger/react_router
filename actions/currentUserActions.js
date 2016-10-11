db323b2431e9b3c874b1d6f02156c97b88e9d273
Chuck Weinberger <chuckhw@sbcglobal.net>
2016-10-11 12:17:30 -0700

    now confirming a reloaded user's auth token with the server prior to restoring that user as the currentUser

--- actions/currentUserActions.js
+++ actions/currentUserActions.js
@@ -1,48 +1,37 @@
+//Note, asynch actions use redux-promise-middleware to automatically dispatch [action-name]_pending/fulfilled/rejected actions across the life of the asynch activity. 
+
 import * as actions from '../constants/actionTypes'
 import axios from 'axios'
 import { authTokenAccepted, insertAuthToken, removeAuthToken } from '../utils/helperMethods'
 
-exports.emailChange = (email='') => ({
-    type: actions.EMAIL_CHANGE,
-    email
-});
-
-exports.usernameChange = (username='') => ({
-    type: actions.USERNAME_CHANGE,
-    username
-});
-
 export const login = data => dispatch => {
+
+//although I want to use redux-promise-middleware for this asynch action, I can't because it auto dispatches the CURRENT_USER_LOGIN_FULLFILLED prior to my inserting the auth token from that newly logged-in user.  I need to insert the auth-token header prior to mounting any component that needs the auth token to fetch data needed by that component.
   
-  dispatch({ type: actions.CURRENT_USER_LOGGING_IN });
-  dispatch({ type: actions.FETCHING });
-  axios.post('/logins', {
-    user: data.username,
-    password: data.password
-  })
-    .then((response) => {
-      localStorage.setItem('currentUser', JSON.stringify(response.data));
-      insertAuthToken(response.data);
-      dispatch({ type: actions.CURRENT_USER_LOGGED_IN, payload: response.data });
-    }) 
-    .catch((err) => {
-      dispatch({ type: actions.CURRENT_USER_LOGIN_FAILURE, payload: err });
-      dispatch({ type: actions.END_FETCHING });
-    })
+  axios.post('/logins', { user: data.username, password: data.password }
+            ).then((response) => {
+                    localStorage.setItem('currentUser', JSON.stringify(response.data));
+                    insertAuthToken(response.data);
+                    dispatch({ type: actions.CURRENT_USER_LOGIN_FULFILLED, payload: response.data })
+            }) 
+            .catch((err) => {
+               dispatch({ type: actions.CURRENT_USER_LOGIN_FAILURE, payload: err });
+            })
 }
 
+//although I want to use redux-promise-middleware for this asynch action, I can't because it auto dispatches the CURRENT_USER_LOGOUT_FULLFILLED prior to my removing the auth token for the logged-out user.  This could cause components that required logged-in users to show there data after the user logs-out.
 export const logout = (data) => dispatch => {
   
-  dispatch({ type: actions.CURRENT_USER_LOGGING_OUT })
+  dispatch({ type: actions.CURRENT_USER_LOGOUT_PENDING })
   dispatch({ type: actions.FETCHING })
   axios.delete('/logins/' + data.currentUser._id )
     .then(() => {
       localStorage.removeItem('currentUser');
       removeAuthToken();
-      dispatch({ type: actions.CURRENT_USER_LOGGED_OUT })
+      dispatch({ type: actions.CURRENT_USER_LOGOUT_FULFILLED })
     }) 
     .catch((err) => {
-      dispatch({ type: actions.CURRENT_USER_LOGOUT_FAILURE, payload: err })
+      dispatch({ type: actions.CURRENT_USER_LOGOUT_REJECTED, payload: err })
       dispatch({ type: actions.END_FETCHING })
     })  
 }
@@ -56,12 +45,25 @@ export const restoreCurrentUser = dispatch => {
                   localStorageUser.auth.accessToken.token;
                   
     if(token){
-      insertAuthToken(localStorageUser)
-      dispatch({ type: actions.CURRENT_USER_LOGGED_IN, payload: localStorageUser })
-    } else return
-};
+//create a axios instance on which we can set a one-team auth header.
+      let instance = axios.create({
+        baseURL: 'http://api.newswick.com/api',
+        headers: { 'Authorization': 'Bearer ' + token }
+      });
+      instance('/logins?access_token=' + token)
+      .then((response) => {
+        insertAuthToken(localStorageUser)
+        dispatch({ type: actions.CURRENT_USER_LOGIN_FULFILLED, payload: localStorageUser })
+      })
+      .catch((error) => {
+        localStorage.removeItem('currentUser');
+        return false
+      }) 
 
+    } else return false
+};
   
+//finally, I get to use Redux-promise-middleware
 export const createNewAccount = (data) => dispatch => {
   
   dispatch({
--- reducers/currentUser.js
+++ reducers/currentUser.js
@@ -2,53 +2,46 @@ import * as actions  from '../constants/actionTypes'
 
 const initialState = {
   currentUser: null,
-  isLoading:false
+  isLoading:false,
+  error: null
 }
 export default function uiReducer(state=initialState, action) {
   
   let modifiedUser = {}
   
   switch (action.type) {
-  // case actions.CURRENT_USER_RESTORE_FROM_LOCAL_STORAGE:
-  //   action.payload.loggedIn = true;
-  //   return { ...state, currentUser: action.payload, isLoading: false }
-  //   break;
-  case actions.CURRENT_USER_LOGGED_IN:
+  case actions.CURRENT_USER_RESTORE_FROM_LOCAL_STORAGE:
     action.payload.loggedIn = true;
     return { ...state, currentUser: action.payload, isLoading: false }
     break;
-  case actions.CURRENT_USER_LOGGED_OUT:
+  case actions.CURRENT_USER_LOGIN_PENDING:
+    return { ...state, isLoading: true }
+    break;
+  case actions.CURRENT_USER_LOGIN_FULFILLED:
+    action.payload.loggedIn = true;
+    return { ...state, currentUser: action.payload, isLoading: false, error: null }
+    break;
+  case actions.CURRENT_USER_LOGIN_REJECTED:
+    return { ...state, isLoading: false, error: action.payload }
+    break;
+  case actions.CURRENT_USER_LOGOUT_PENDING:
+    return { ...state, isLoading: true }
+    break;
+  case actions.CURRENT_USER_LOGOUT_FULFILLED:
     return initialState
     break;
-  case actions.CURRENT_USER_LOGGING_IN:
-    return { ...state, isLoading: true }
-    break;
-  case actions.CURRENT_USER_LOGGING_OUT:
-    return { ...state, isLoading: true }
-    break;
-  case actions.CURRENT_USER_LOGIN_FAILURE:
-    return { ...state, isLoading: false }
-    break;
-  case actions.CURRENT_USER_LOGOUT_FAILURE:
-    return { ...state, isLoading: false }
+  case actions.CURRENT_USER_LOGOUT_REJECTED:
+    return { ...state, isLoading: false, error: action.payload }
     break;
   case actions.CREATE_ACCOUNT_PENDING:
     return { ...state, isLoading: true }
     break;
-  case actions.CREATE_ACCOUNT_REJECTED:
-    return { ...state, isLoading: false }
-    break;
   case actions.CREATE_ACCOUNT_FULFILLED:
     action.payload.loggedIn = true;
-    return { ...state, currentUser: action.payload, isLoading: false, }
+    return { ...state, currentUser: action.payload, isLoading: false, error: null }
     break;
-  case actions.EMAIL_CHANGE:
-    modifiedUser = { ...state.currentUser, email: action.payload }
-    return { ...state, modifiedUser };
-    break;
-  case actions.USERNAME_CHANGE:
-    modifiedUser = { ...state.currenUser, username: action.payload }
-    return { ...state, modifiedUser };
+  case actions.CREATE_ACCOUNT_REJECTED:
+    return { ...state, isLoading: false, error: action.payload }
     break;
   default:
     return state;
